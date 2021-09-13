7f1be8a72d0f135cdd0c0df7e9528fc8b4c42cfd
arzehgar_a <arzehgar_a@office.zharfpouyan.net>
2021-09-13 11:55:21 +0430

    Add modernize lib to project

---
+++ modernize/src/main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-09-13
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "modernize/main.h"
